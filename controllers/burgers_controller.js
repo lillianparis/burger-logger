8ee935e0841f63b221b21abd69d7b95c1f0eca42
Lillian Paris <lillian.paris529@gmail.com>
2020-11-09 21:26:01 -0700

    Created controller using activity

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model to use its database functions.
+const cat = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+    cat.all(function (data) {
+        const hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burger", function (req, res) {
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    const devoured = "id = " + req.params.id;
+
+    console.log("devoured", devoured);
+
+    burger.update(
+        {
+            devoured: req.body.devoured
+        },
+        devoured,
+        function (result) {
+            if (result.changedRows === 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            }
+            res.status(200).end();
+
+        }
+    );
+});
+
+// Export routes for server.js to use.
+module.exports = router;
