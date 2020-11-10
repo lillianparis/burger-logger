f2d056fae0277790d101ad71c7c54cb1f72ca497
Lillian Paris <lillian.paris529@gmail.com>
2020-11-09 20:58:08 -0700

    Added orm.js from refernce class activity

--- config/connection.js
+++ config/connection.js
@@ -3,20 +3,20 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "burgers_db",
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db",
 });
 
 // Make connection.
 connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
