831be6fce3ea74e300b7b6061cbb9f9a3b717e26
Lillian Paris <lillian.paris529@gmail.com>
2020-11-09 22:59:37 -0700

    Downloaded installers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,5 @@
-INSERT INTO burgers (burger_name)
-VALUES ("Vegan Burger");
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
 
-INSERT INTO burgers (burger_name)
-VALUES ("Single burger");
+INSERT INTO burgers (burger_name) VALUES ('Single burger');
 
-INSERT INTO burgers (burger_name)
-VALUES ("Double burger");
+INSERT INTO burgers (burger_name) VALUES ('Double burger');
