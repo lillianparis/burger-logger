f5d8560158c734aac3d331f12e973b2f0d9bc012
Lillian Paris <lillian.paris529@gmail.com>
2021-01-09 11:03:01 -0700

    Added

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,17 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'rootroot',
+        database: 'burgers_db'
+    });
+};
+
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
